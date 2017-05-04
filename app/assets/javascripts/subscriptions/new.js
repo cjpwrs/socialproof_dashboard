82c7a571d63320067612400a3a3396c206caf525
chacharoti <chacharoti@gmail.com>
2017-05-03 23:43:36 -0700

    Prevent double submit

--- app/assets/javascripts/subscriptions/new.js
+++ app/assets/javascripts/subscriptions/new.js
@@ -6,19 +6,24 @@ $(function() {
 var stripe_initialize = function() {
   var $form = $('#new_subscription');
   $form.submit(function(event) {
+    $('input[type=submit]').attr('disabled', true)
     Stripe.card.createToken({
       number: $('#subscription_card_number').val(),
       cvc: $('#subscription_card_cvc').val(),
       exp: $('#subscription_card_expiry').val()
     }, stripeResponseHandler);
+    return false;
   });
 };
 
 function stripeResponseHandler(status, response) {
-  console.log(status);
+  var $form = $('#new_subscription');
   if (status === 200) {
-    return alert(response.id);
+    var token = response.id;
+    $('#subscription_stripe_card_token').val(response.id);
+    $form.get(0).submit();
   } else {
+    $('input[type=submit]').attr('disabled', false)
     return alert(response.error.message);
   }
 };
--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -16,19 +16,7 @@ class SubscriptionsController < ApplicationController
 
   def create
     # Amount in cents
-    @amount = 500
-
-    customer = Stripe::Customer.create(
-      :email => params[:stripeEmail],
-      :source  => params[:stripeToken]
-    )
-
-    charge = Stripe::Charge.create(
-      :customer    => customer.id,
-      :amount      => @amount,
-      :description => 'Rails Stripe customer',
-      :currency    => 'usd'
-    )
+    p params
 
   rescue Stripe::CardError => e
     flash[:error] = e.message
