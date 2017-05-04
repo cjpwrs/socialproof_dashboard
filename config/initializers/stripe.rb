3a5ca9cd18423780c4e86838ffc2f013eb4cfc0c
chacharoti <chacharoti@gmail.com>
2017-05-03 23:33:03 -0700

    Send cc card to stripe

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,5 +1,5 @@
 Rails.configuration.stripe = {
-  publishable_key: ENV['STRIPE_PUB_KEY'],
-  secret_key: ENV['STRIPE_KEY']
+  publishable_key: 'pk_test_OfdmQReUz0BqhqUjAuMK4x7F',
+  secret_key: 'sk_test_Qw8ABuEoT6Fxu2ZMzFYSEWQs'
 }
-Stripe.api_key = ENV['STRIPE_KEY']
+Stripe.api_key = 'sk_test_Qw8ABuEoT6Fxu2ZMzFYSEWQs'
