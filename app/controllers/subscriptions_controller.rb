ed0ce4ba5be93edc8754045fd12a0bb8e92a3f28
chacharoti <chacharoti@gmail.com>
2017-05-04 09:33:34 -0700

    Display subscription info

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -10,7 +10,7 @@ class SubscriptionsController < ApplicationController
   def create
     plan_id = subscriptions_params['plan']
     plan = Stripe::Plan.retrieve(plan_id)
-    
+
     customer = Stripe::Customer.create(
                   :description => "Customer for #{current_user.email}",
                   :source => subscriptions_params['stripe_card_token'],
@@ -18,9 +18,9 @@ class SubscriptionsController < ApplicationController
                 )
     stripe_subscription = customer.subscriptions.create(:plan => plan.id)
 
-    @subscription = current_user.subscriptions.new(stripe_subscription_id: stripe_subscription)
+    @subscription = current_user.subscriptions.new(stripe_subscription_id: stripe_subscription.id)
     if @subscription.save
-      redirect_to root_path
+      redirect_to @subscription
     else
       render :new_subscription
     end
@@ -29,8 +29,22 @@ class SubscriptionsController < ApplicationController
     redirect_to new_subscription_path
   end
 
+  def show
+    @subscription = get_subscription
+    @strip_subscription_json = Stripe::Subscription.retrieve @subscription.stripe_subscription_id
+    @card = Stripe::Customer.retrieve(@strip_subscription_json.customer).sources.data
+  end
+
   private
   def subscriptions_params
     params.require(:subscription).permit(:plan, :stripe_card_token)
+  end
+
+  def subscription_id
+    params['id']
+  end
+
+  def get_subscription
+    Subscription.find subscription_id
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :subscriptions
+
+  def is_subscripted?
+    self.subscriptions.any?
+  end
+
+  def lastest_subscription
+    self.subscriptions.order(created_at: :asc).last
+  end
 end
