eea505f0ba2a6d6baa69d76acdd1827409ca0c51
chacharoti <chacharoti@gmail.com>
2017-07-23 13:44:42 -0700

    Upgrade plan

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -70,15 +70,64 @@ class SubscriptionsController < ApplicationController
     end
   end
 
+  def upgrade_plan
+    @subscription = Subscription.new
+    @stripe_list = (Stripe::Plan.all).sort_by { |plan| plan[:amount] }
+    subscriptions = current_user.subscriptions.order(created_at: :desc)
+    @plans =
+      if subscriptions.size == 0
+        redirect_to :root
+      else
+        @subscription = subscriptions.first
+        @strip_subscription_json = Stripe::Subscription.retrieve @subscription.stripe_subscription_id
+        @stripe_list.reject!{ |plan| plan.amount <= @strip_subscription_json.plan.amount }.map{ |plan|
+          price = plan[:amount].to_f/100
+          coupon = Stripe::Coupon.retrieve(coupon_finder(plan))
+          discount = coupon[:amount_off].to_f/100
+          price_with_discount = price - discount
+
+          [plan[:name] + ' - ' +  number_to_currency(price_with_discount, precision: 0) +
+          ' for 30 days. After 30 days, ' + number_to_currency(price, precision: 0) + '/month', plan[:id]]
+        }
+      end
+  end
+
+  def upgrade
+    plan_id = subscriptions_params['plan']
+    plan = Stripe::Plan.retrieve(plan_id)
+    subscription = get_subscription
+    strip_subscription_json = Stripe::Subscription.retrieve subscription.stripe_subscription_id
+    customer = Stripe::Customer.retrieve(strip_subscription_json.customer)
+    coupon = current_user.subscriptions.count == 0 ? coupon_finder(plan) : nil
+
+    stripe_subscription = customer.subscriptions.create(plan: plan.id, coupon: coupon)
+
+    @subscription = current_user.subscriptions.new(stripe_subscription_id: stripe_subscription.id, status: stripe_subscription.status)
+    if @subscription.save
+      tracker do |t|
+        t.google_adwords_conversion :conversion, { label: 'zAPvCNHs7XAQ0tvHmAM' }
+        t.facebook_pixel :track, { type: 'Purchase', options: { value: 247.35, currency: 'USD' } }
+      end
+      cancel_subscription
+      redirect_to @subscription
+    else
+      render :upgrade_plan
+    end
+  end
+
   def cancel
-    subscription = current_user.subscriptions.order(id: :asc).where(id: subscription_id).first
-    strip_subscription = Stripe::Subscription.retrieve(subscription.stripe_subscription_id)
-    strip_subscription.delete(:at_period_end => false)
-    subscription.update_attributes(status: strip_subscription.status)
+    cancel_subscription
     redirect_to new_subscription_path
   end
 
   private
+  def cancel_subscription
+    subscription = current_user.subscriptions.order(id: :asc).where(id: subscription_id).first
+    strip_subscription = Stripe::Subscription.retrieve(subscription.stripe_subscription_id)
+    strip_subscription.delete(:at_period_end => false)
+    subscription.update_attributes(status: strip_subscription.status)
+  end
+
   def subscriptions_params
     params.require(:subscription).permit(:plan, :stripe_card_token)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Rails.application.routes.draw do
   resources :subscriptions do
     member do
       post 'cancel'
+      get 'upgrade_plan'
+      post 'upgrade'
     end
   end
 
