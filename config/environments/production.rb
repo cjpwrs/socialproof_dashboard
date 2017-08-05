c5d305191251a616d19199d8a28d08379352022b
chacharoti <chacharoti@gmail.com>
2017-08-05 03:52:43 -0700

    Add mailer host env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,8 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "social_proof_#{Rails.env}"
   config.action_mailer.perform_caching = false
-
+  
+  config.action_mailer.default_url_options = { host: ENV.fetch('MAILER_HOST') }
   ActionMailer::Base.smtp_settings = {
       :address        => 'smtp.sendgrid.net',
       :port           => '587',
